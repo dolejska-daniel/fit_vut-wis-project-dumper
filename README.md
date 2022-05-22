d61b9f07159f018e672569d07531080a673aa17b
Daniel Dolejška <dolejska-daniel@users.noreply.github.com>
2022-05-22 11:52:04 +0200

    docs: add README.md, improve argument naming

---
+++ README.md
@@ -0,0 +1,15 @@
+# FIT VUT WIS Project Dumper
+
+Requirements:
+- `Python >=3.9`
+- `pipenv`
+
+### Installation
+```
+pipenv install
+```
+
+### Usage
+```
+pipenv run python main.py [-v[v]] [-o|--output OUTPUT_DIR]
+```
--- main.py
+++ main.py
@@ -13,10 +13,10 @@ def main(output_dir: Path):
 
 
 if __name__ == '__main__':
-    output_default = f"{os.getcwd()}/wis_projects"
+    default_dir = f"{os.getcwd()}/wis_projects"
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("-o", "--output", action="store", dest="output_dir", default=output_default)
+    parser.add_argument("-o", "--output", action="store", dest="output_dir", metavar="OUTPUT_DIR", default=default_dir)
     parser.add_argument("-v", action="count", dest="version", default=0)
     args = parser.parse_args()
 
